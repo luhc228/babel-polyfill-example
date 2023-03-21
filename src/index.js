0f1a301cd46b019e7c3d156b819c92ef98a3923c
luhc228 <luhengchang228@gmail.com>
2023-03-21 21:27:51 +0800

    feat: add @babel/plugin-transform-runtime

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+// const add = async (a, b) => {
+//   return Promise.resolve(a + b);
+// };
+
+// add(1, 2).then((res) => console.log(res));
+
 const add = async (a, b) => {
-  return Promise.resolve(a + b);
+  return Promise.resolve({ res: a + b, succuss: true });
 };
 
-add(1, 2).then((res) => console.log(res));
+add(1, 2).then((res) => console.log({ ...res }));
+
